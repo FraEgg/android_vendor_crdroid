4826fd7b82a3f6e9de05fd1dd232e4bc506132e8
Frank Eggen <eggen@emden.net>
2020-01-03 14:05:00 +0100

    Cleanup LuisWuff for Q

--- config/lineage_audio.mk
+++ config/lineage_audio.mk
@@ -33,3 +33,4 @@ PRODUCT_COPY_FILES += \
     $(RINGTONE_PATH)/Rockin.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Rockin.ogg \
     $(RINGTONE_PATH)/Sheep.mp3:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Sheep.mp3 \
     $(RINGTONE_PATH)/Yukaay.ogg:$(TARGET_COPY_OUT_PRODUCT)/media/audio/ringtones/Yukaay.ogg
+

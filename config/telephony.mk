622c59ca4834b13f642fadaac7a5d51caea15326
Frank Eggen <eggen@emden.net>
2020-01-07 17:24:00 +0100

    set LuisWuff as default

--- config/telephony.mk
+++ config/telephony.mk
@@ -14,8 +14,8 @@ PRODUCT_PACKAGES += \
 
 # Default ringtone
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.config.ringtone=Orion.ogg
-
+    ro.config.ringtone=LuisWuff.ogg
+	
 # Tethering - allow without requiring a provisioning app
 # (for devices that check this)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
